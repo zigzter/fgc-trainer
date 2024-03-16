692815295562951a611ffc53c3b9a7d60262146b
Ziggy <zigs_17@hotmail.com>
2024-03-15 20:22:39 -0700

    Add skeleton sign-up/in pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import SignUp from "./features/auth/SignUp.tsx";
+import SignIn from "./features/auth/SignIn.tsx";
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -17,6 +19,14 @@ const router = createBrowserRouter([
     {
         index: true,
         element: <App />,
+    },
+    {
+        path: "signup",
+        element: <SignUp />,
+    },
+    {
+        path: "signin",
+        element: <SignIn />,
     },
 ]);
 
