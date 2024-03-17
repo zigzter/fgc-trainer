26cbef6dd546b3eed2a829d727d1c29bae3aba0d
Ziggy <zigs_17@hotmail.com>
2024-03-16 17:54:58 -0700

    Add MaterialUI, React Hook Form & Roboto

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
-import "./index.css";
 import SignUp from "./features/auth/SignUp.tsx";
 import SignIn from "./features/auth/SignIn.tsx";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const queryClient = new QueryClient({
     defaultOptions: {
