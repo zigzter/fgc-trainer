d628eeb286b0fc55e4e7504111e27e05a3d03a4b
Ziggy <zigs_17@hotmail.com>
2024-04-25 18:27:15 -0700

    Add JWT retrieval helper

--- frontend/src/utils/user.ts
+++ frontend/src/utils/user.ts
@@ -1,9 +1,22 @@
-import { getCurrentUser } from "aws-amplify/auth";
+import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
 
 export async function getUser() {
     try {
         const { userId, username } = await getCurrentUser();
         return { userId, username };
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
+}
+
+export async function getJWT() {
+    try {
+        const { accessToken } = (await fetchAuthSession()).tokens ?? {};
+        return {
+            payload: accessToken?.payload,
+            string: accessToken?.toString(),
+        };
     } catch (e) {
         console.error(e);
         return null;
