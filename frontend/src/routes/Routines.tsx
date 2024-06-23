70afc1d45fce99d7bd4a4e93ef2e5374f0b483e1
Ziggy <zigs_17@hotmail.com>
2024-06-23 16:59:40 -0700

    Close routine form on successful creation

--- frontend/src/routes/Routines.tsx
+++ frontend/src/routes/Routines.tsx
@@ -1,16 +1,27 @@
 import { useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
 import { Button } from "@mui/material";
 import RoutineForm from "../components/RoutineForm";
 import RoutinesList from "../components/RoutinesList";
 
 export default function Routines() {
     const [isCreating, setIsCreating] = useState(false);
+    const queryClient = useQueryClient();
+
+    const onSuccess = () => {
+        queryClient.invalidateQueries({ queryKey: ["routines"] });
+        setIsCreating(false);
+    };
 
     return (
         <>
             <h1>Routines</h1>
             {isCreating ? (
-                <RoutineForm onCancel={() => setIsCreating(false)} method="POST" />
+                <RoutineForm
+                    onSuccess={onSuccess}
+                    onCancel={() => setIsCreating(false)}
+                    method="POST"
+                />
             ) : (
                 <Button onClick={() => setIsCreating(true)}>New Routine</Button>
             )}
