90725cc8e0e73a81bb480d1406cd100662b55e9d
Ziggy <zigs_17@hotmail.com>
2024-10-23 15:55:51 -0700

    Add basic session retrieval to history

--- frontend/src/routes/History.tsx
+++ frontend/src/routes/History.tsx
@@ -1,3 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import { getRoutineSessions } from "../api/routine_sessions";
+import { CircularProgress } from "@mui/material";
+
 export default function History() {
-    return <h1>History</h1>;
+    const {
+        data: history,
+        error,
+        isError,
+        isPending,
+    } = useQuery({
+        queryKey: ["history"],
+        queryFn: getRoutineSessions,
+    });
+
+    if (isPending) {
+        return (
+            <div>
+                <CircularProgress />
+            </div>
+        );
+    }
+
+    if (isError) {
+        return <p>{error.message}</p>;
+    }
+
+    return (
+        <>
+            <h1>History</h1>
+            {history.map((session) => (
+                <p>{session.id}</p>
+            ))}
+        </>
+    );
 }
