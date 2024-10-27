80994da140dc8ee72e7aeb733b7a55e814daf91a
Ziggy <zigs_17@hotmail.com>
2024-10-27 11:27:25 -0700

    Add formatDate and vitest

---
+++ frontend/src/utils/formatDate.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "vitest";
+import { formatDate } from "./formatDate";
+
+const railsDate = "2024-10-26T19:07:25.466Z";
+
+describe("formatDate tests", () => {
+    it("formats a Rails date", () => {
+        const result = formatDate(railsDate);
+        expect(result).toBe("10/26/2024, 12:07:25 PM");
+    });
+});
---
+++ frontend/src/utils/formatDate.ts
@@ -0,0 +1,11 @@
+export const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+    }).format(date);
+};
